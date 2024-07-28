6e6326b9d288eee65606f0db49ec27f3bbe2c5dd
Vanderlei Loureiro <vanderloureiroleite@gmail.com>
2024-07-28 16:41:24 -0300

    mudanca de map para list e invalidando todo cache

--- src/main/java/dev/vanderloureiro/springboot_cache_std/ExemploResource.java
+++ src/main/java/dev/vanderloureiro/springboot_cache_std/ExemploResource.java
@@ -1,33 +1,45 @@
 package dev.vanderloureiro.springboot_cache_std;
 
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
+import java.util.List;
 
 @RestController
 public class ExemploResource {
 
-    Map<Long, Filme> filmes = Map.of(
-            1L, new Filme("1", "1"),
-            2L, new Filme("2", "2"),
-            3L, new Filme("3", "3"),
-            4L, new Filme("4", "4")
-    );
+    List<Filme> filmes = List.of(
+            new Filme(1L, "1", "1"),
+            new Filme(2L, "2", "2"),
+            new Filme(3L, "3", "3"),
+            new Filme(4L, "4", "4"));
 
     @Cacheable("filmes")
     @GetMapping("/{id}")
-    public ResponseEntity<Filme> get(@PathVariable Long id) {
-        return ResponseEntity.ok(buscaPorId(id));
+    public ResponseEntity<Filme> buscar(@PathVariable Long id) {
+        return ResponseEntity.ok(buscarPorId(id));
+    }
+
+//    @CacheEvict(value = "filmes", key = "id")
+//    @GetMapping("/limpar-unico/{id}")
+//    public void limparCache(@PathVariable Long id) {
+//
+//    }
+
+    @CacheEvict(value = "filmes", allEntries = true)
+    @GetMapping("/limpar-todos")
+    public void limparTodos() {
+
     }
     
-    private Filme buscaPorId(Long id) {
+    private Filme buscarPorId(Long id) {
         try {
             Thread.sleep(1000);
-            return filmes.get(id);
+            return filmes.stream().filter(filme -> filme.id().equals(id)).findFirst().get();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/dev/vanderloureiro/springboot_cache_std/Filme.java
+++ src/main/java/dev/vanderloureiro/springboot_cache_std/Filme.java
@@ -1,4 +1,4 @@
 package dev.vanderloureiro.springboot_cache_std;
 
-public record Filme(String titulo, String diretor) {
+public record Filme(Long id, String titulo, String diretor) {
 }
