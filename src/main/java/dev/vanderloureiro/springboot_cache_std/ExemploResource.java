827b9ae0467fe37eff427dcc385b03e7978dddc5
Vanderlei Loureiro <vanderloureiroleite@gmail.com>
2024-07-28 15:22:05 -0300

    estrutura de map como banco de dados

--- src/main/java/dev/vanderloureiro/springboot_cache_std/ExemploResource.java
+++ src/main/java/dev/vanderloureiro/springboot_cache_std/ExemploResource.java
@@ -1,7 +1,24 @@
 package dev.vanderloureiro.springboot_cache_std;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class ExemploResource {
+
+    Map<Long, Filme> filmes = Map.of(
+            1L, new Filme("1", "1"),
+            2L, new Filme("2", "2"),
+            3L, new Filme("3", "3"),
+            4L, new Filme("4", "4")
+    );
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Filme> get(@PathVariable Long id) {
+        return ResponseEntity.ok(filmes.get(id));
+    }
 }
---
+++ src/main/java/dev/vanderloureiro/springboot_cache_std/Filme.java
@@ -0,0 +1,4 @@
+package dev.vanderloureiro.springboot_cache_std;
+
+public record Filme(String titulo, String diretor) {
+}
